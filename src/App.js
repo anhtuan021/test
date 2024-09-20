76e75eb768aa48d3d70aa9396f62527ba3cc0ca8
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2024-09-20 13:13:40 +0700

    lab2

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         </a>
       </header>
       <div class="mb-3">
+        
   <label for="exampleFormControlInput1" class="form-label">Email address</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
 </div>
